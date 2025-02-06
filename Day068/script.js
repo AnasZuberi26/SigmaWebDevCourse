43da04b451735e363873fb74418c2f0ea3f5f6fd
AnasZuberi26 <anas26092001@gmail.com>
2025-02-06 16:16:38 +0500

    5-Feb

---
+++ Day068/script.js
@@ -0,0 +1,26 @@
+console.log("Anas Zuberi");
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = 'red'
+
+// document.getElementById("redbox").style.backgroundColor = 'red'
+
+// document.querySelector(".box").style.backgroundColor = "green"
+
+// console.log(document.querySelectorAll(".box"));
+// document.querySelectorAll(".box").forEach((element) => {
+//   element.style.backgroundColor = "green";
+// });
+
+// console.log(document.getElementsByTagName("div"))
+
+// console.log(document.getElementsByTagName("div")[4].matches("#redbox"))
+// console.log(document.getElementsByTagName("div")[3].matches("#redbox"))
+
+// console.log(document.getElementsByTagName("div")[3].closest("#redbox"))
+// console.log(document.getElementsByTagName("div")[3].closest(".container"))
+
+// console.log(document.querySelector(".container").contains(document.getElementsByTagName("div")[2]))
+// console.log(document.querySelector(".container").contains(document.querySelector("body")))
