f63740469123e269c20433b9ebf0d12ddca06764
AnasZuberi26 <anas26092001@gmail.com>
2025-02-08 20:03:04 +0500

    8-Feb

--- Day075/promise.js
+++ Day075/promise.js
@@ -1,10 +1,16 @@
 console.log("This is promise");
 
 let prom1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("Done");
-    resolve("Anas");
-  }, 3000);
+  4;
+  let a = Math.random();
+  if (a < 0.5) {
+    reject("No random number was supporting you");
+  } else {
+    setTimeout(() => {
+      console.log("Done");
+      resolve("Anas");
+    }, 3000);
+  }
 });
 
 prom1.then((a) => {
