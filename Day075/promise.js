f113cc0c95f3313af214bb2b0acbf840e32e18ca
AnasZuberi26 <anas26092001@gmail.com>
2025-02-08 20:04:01 +0500

    8-Feb

--- Day075/promise.js
+++ Day075/promise.js
@@ -1,7 +1,6 @@
 console.log("This is promise");
 
 let prom1 = new Promise((resolve, reject) => {
-  4;
   let a = Math.random();
   if (a < 0.5) {
     reject("No random number was supporting you");
