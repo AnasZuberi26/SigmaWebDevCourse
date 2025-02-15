c0d53c702fb6d359e77c66accfcac764ec70f1ad
AnasZuberi26 <anas26092001@gmail.com>
2025-02-15 20:28:20 +0500

    15-Feb

--- Day084/script.js
+++ Day084/script.js
@@ -11,7 +11,7 @@ async function getSongs() {
   for (let i = 0; i < as.length; i++) {
     const element = as[i];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href);
+      songs.push(element.href.split("/songs/")[1]);
     }
   }
   return songs;
@@ -19,11 +19,25 @@ async function getSongs() {
 
 async function main() {
   let songs = await getSongs();
-  console.log(songs);
 
-  //   Play the first song
-  var audio = new Audio(songs[0]);
-  audio.play();
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li> 
+                <img class="invert" src="music.svg" alt="" />
+                <div class="info">
+                  <div>${song.replaceAll("%20", " ")}</div>
+                  <div>Anonymous</div>
+                </div>
+                <div class="playnow">
+                  <span>Play Now</span>
+                  <img class="invert" src="play.svg" alt="" />
+                </div>
+     </li>`;
+  }
 }
 
 main();
