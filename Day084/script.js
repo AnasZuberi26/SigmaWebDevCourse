5d8abf5a7361a816ad13e3fbb40ea085734a346d
AnasZuberi26 <anas26092001@gmail.com>
2025-02-17 16:33:44 +0500

    17-Feb

--- Day084/script.js
+++ Day084/script.js
@@ -105,9 +105,14 @@ async function main() {
   });
 
   // Add an event listener for hamburger
-  document.querySelector(".hamburger").addEventListener("click", ()=>{
-    document.querySelector(".left").style.left = "0"
-  })
+  document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".left").style.left = "0";
+  });
+
+  // Add an event listener for close button
+  document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".left").style.left = "-120%";
+  });
 }
 
 main();
