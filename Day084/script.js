1318943c51708f6d09bb69b18c7d7c805fd599d9
AnasZuberi26 <anas26092001@gmail.com>
2025-02-18 20:43:07 +0500

    18-Feb

--- Day084/script.js
+++ Day084/script.js
@@ -26,33 +26,19 @@ async function getSongs(folder) {
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
-  let songs = [];
+  songs = [];
   for (let i = 0; i < as.length; i++) {
     const element = as[i];
     if (element.href.endsWith(".mp3")) {
       songs.push(element.href.split(`${folder}/`)[1]);
     }
   }
-  return songs;
-}
 
-const playMusic = (track, pause = false) => {
-  currentSong.src = `/Day084/${currfolder}/` + track;
-  if (!pause) {
-    currentSong.play();
-    play.src = "pause.svg";
-  }
-  document.querySelector(".songinfo").innerHTML = decodeURI(track);
-  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
-};
-
-async function main() {
-  songs = await getSongs("songs/ncs2");
-  playMusic(songs[0], true);
-
+  // Show all songs in library
   let songUL = document
     .querySelector(".songList")
     .getElementsByTagName("ul")[0];
+  songUL.innerHTML = "";
   for (const song of songs) {
     songUL.innerHTML =
       songUL.innerHTML +
@@ -78,6 +64,82 @@ async function main() {
       playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
     });
   });
+}
+
+const playMusic = (track, pause = false) => {
+  currentSong.src = `/Day084/${currfolder}/` + track;
+  if (!pause) {
+    currentSong.play();
+    play.src = "pause.svg";
+  }
+  document.querySelector(".songinfo").innerHTML = decodeURI(track);
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+};
+
+async function displayAlbum() {
+  let a = await fetch(`http://127.0.0.1:3000/Day084/songs/`);
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let anchors = div.getElementsByTagName("a");
+  let cardContainer = document.querySelector(".cardContainer");
+  let array = Array.from(anchors);
+  for (let index = 0; index < array.length; index++) {
+    const e = array[index];
+
+    if (e.href.includes("/songs")) {
+      let folder = e.href.split("/").slice(-2)[0];
+      // Get the meta data of the folder
+      let a = await fetch(
+        `http://127.0.0.1:3000/Day084/songs/${folder}/info.json`
+      );
+      let response = await a.json();
+      // console.log(response);
+      cardContainer.innerHTML =
+        cardContainer.innerHTML +
+        ` <div data-folder="${folder}" class="card">
+      <div class="play">
+                <svg
+                width="16"
+                height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                <path
+                d="M5 20V4L19 12L5 20Z"
+                    stroke="#141B34"
+                    fill="#000"
+                    stroke-width="1.5"
+                    stroke-linejoin="round"
+                    />
+                </svg>
+              </div>
+              <img
+              src="/Day084/songs/${folder}/cover.jpg"
+                alt=""
+              />
+              <h2>${response.title}</h2>
+              <p>${response.description}</p>
+            </div>`;
+    }
+  }
+  // console.log(anchors);
+
+  // Load the playlist whenever card is clicked
+  Array.from(document.getElementsByClassName("card")).forEach((e) => {
+    e.addEventListener("click", async (item) => {
+      songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
+    });
+  });
+}
+
+async function main() {
+  await getSongs("songs/ncs");
+  playMusic(songs[0], true);
+
+  // Display all the albums on the page
+  await displayAlbum();
 
   // Attach an event listener to control the music
   play.addEventListener("click", () => {
@@ -147,6 +209,17 @@ async function main() {
       // console.log(e, e.target, e.target.value);
       currentSong.volume = parseInt(e.target.value) / 100;
     });
+
+  // Add event listener to mute the track
+  document.querySelector(".volume>img").addEventListener("click", (e) => {
+    if (e.target.src.includes("volume.svg")) {
+      e.target.src = e.target.src.replace("volume.svg", "mute.svg");
+      currentSong.volume = 0;
+    } else {
+      e.target.src = e.target.src.replace("mute.svg", "volume.svg");
+      currentSong.volume = 0.3;
+    }
+  });
 }
 
 main();
